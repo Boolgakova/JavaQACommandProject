c26665d4e7c94698269687a0350b07f218608ce1
Boolgakova <boolgakova@gmail.com>
2022-07-18 10:01:20 +0300

    Back to the previous version

--- src/test/java/ru/netology/PlayerTest.java
+++ src/test/java/ru/netology/PlayerTest.java
@@ -12,162 +12,162 @@ public class PlayerTest {
     Game game3 = store.publishGame("Морской бой", "Аркады");
     Game game4 = store.publishGame("Лесной бой", "Стратегия");
 
-//    @Test
-//    public void shouldInstallNewGame() {
-//
-//        Player player = new Player("Ann");
-//        player.installGame(game);
-//
-//        boolean expected = true;
-//        boolean actual = player.playedTime.containsKey(game);
-//
-//        assertEquals(expected, actual);
-//    }
-//
-//    @Test
-//    public void shouldNotFindNotInstalledGame() {
-//
-//        Player player = new Player("Ann");
-//
-//        boolean expected = false;
-//        boolean actual = player.playedTime.containsKey(game);
-//
-//        assertEquals(expected, actual);
-//    }
-//
-//    @Test
-//    public void shouldNothingHappenWhenInstallExistingGame() {
-//
-//        Player player = new Player("Ann");
-//        player.installGame(game);
-//        player.installGame(game);
-//
-//        boolean expected = true;
-//        boolean actual = player.playedTime.containsKey(game);
-//
-//        assertEquals(expected, actual);
-//    }
-//
-//    @Test
-//    public void shouldSumTimeOfNewGame() {
-//
-//        Player player = new Player("Ann");
-//        player.installGame(game);
-//        player.play(game, 1);
-//
-//        int expected = 1;
-//        int actual = player.playedTime.get(game);
-//
-//        assertEquals(expected, actual);
-//    }
-//
-//    @Test
-//    public void shouldSumTimeOfOldGame() {
-//
-//        Player player = new Player("Ann");
-//        player.installGame(game);
-//        player.play(game, 1);
-//        player.play(game, 3);
-//        player.play(game, 2);
-//
-//        int expected = 6;
-//        int actual = player.playedTime.get(game);
-//
-//        assertEquals(expected, actual);
-//    }
-//
-//    @Test
-//    public void shouldThrowRuntimeExceptionWhenGameNotInstall() {
-//
-//        Player player = new Player("Ann");
-//
-//        assertThrows(RuntimeException.class, () -> {
-//            player.play(game, 1);
-//        });
-//    }
-//
-//    @Test
-//    public void shouldSumGenreIfOneGame() {
-//
-//        Player player = new Player("Petya");
-//        player.installGame(game1);
-//        player.play(game1, 3);
-//
-//        int expected = 3;
-//        int actual = player.sumGenre(game1.getGenre());
-//        assertEquals(expected, actual);
-//    }
-//
-//    @Test
-//    public void shouldSumGenreInSeveralGames() {
-//
-//        Player player = new Player("Petya");
-//        player.installGame(game1);
-//        player.installGame(game2);
-//        player.installGame(game3);
-//        player.play(game1, 3);
-//        player.play(game2, 1);
-//        player.play(game3, 4);
-//
-//        int expected = 8;
-//        int actual = player.sumGenre(game1.getGenre());
-//
-//        assertEquals(expected, actual);
-//    }
-//
-//    @Test
-//    public void shouldSumGenreInSeveralGamesWithRepeat() {
-//
-//        Player player = new Player("Petya");
-//        player.installGame(game1);
-//        player.installGame(game2);
-//        player.installGame(game3);
-//        player.play(game1, 3);
-//        player.play(game2, 1);
-//        player.play(game3, 4);
-//        player.play(game1, 2);
-//
-//        int expected = 10;
-//        int actual = player.sumGenre(game1.getGenre());
-//
-//        assertEquals(expected, actual);
-//    }
-//
-//    @Test
-//    public void shouldNotSumDifferentGenre() {
-//
-//        Player player = new Player("Petya");
-//        player.installGame(game1);
-//        player.installGame(game2);
-//        player.installGame(game4);
-//        player.play(game1, 3);
-//        player.play(game2, 1);
-//        player.play(game4, 4);
-//
-//        int expected = 4;
-//        int actual = player.sumGenre(game1.getGenre());
-//
-//        assertEquals(expected, actual);
-//    }
+    @Test
+    public void shouldInstallNewGame() {
 
-//    @Test
-//    public void shouldNotSumGenreOfDifferentPlayer() {
-//
-//        Player player = new Player("Petya");
-//        player.installGame(game1);
-//        player.installGame(game2);
-//        player.play(game1, 3);
-//        player.play(game2, 1);
-//
-//        Player player1 = new Player("Ann");
-//        player1.installGame(game2);
-//        player1.play(game2, 4);
-//
-//        int expected = 4;
-//        int actual = player.sumGenre(game2.getGenre());
-//
-//        assertEquals(expected, actual);
-//    }
+        Player player = new Player("Ann");
+        player.installGame(game);
+
+        boolean expected = true;
+        boolean actual = player.playedTime.containsKey(game);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldNotFindNotInstalledGame() {
+
+        Player player = new Player("Ann");
+
+        boolean expected = false;
+        boolean actual = player.playedTime.containsKey(game);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldNothingHappenWhenInstallExistingGame() {
+
+        Player player = new Player("Ann");
+        player.installGame(game);
+        player.installGame(game);
+
+        boolean expected = true;
+        boolean actual = player.playedTime.containsKey(game);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSumTimeOfNewGame() {
+
+        Player player = new Player("Ann");
+        player.installGame(game);
+        player.play(game, 1);
+
+        int expected = 1;
+        int actual = player.playedTime.get(game);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSumTimeOfOldGame() {
+
+        Player player = new Player("Ann");
+        player.installGame(game);
+        player.play(game, 1);
+        player.play(game, 3);
+        player.play(game, 2);
+
+        int expected = 6;
+        int actual = player.playedTime.get(game);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldThrowRuntimeExceptionWhenGameNotInstall() {
+
+        Player player = new Player("Ann");
+
+        assertThrows(RuntimeException.class, () -> {
+            player.play(game, 1);
+        });
+    }
+
+    @Test
+    public void shouldSumGenreIfOneGame() {
+
+        Player player = new Player("Petya");
+        player.installGame(game1);
+        player.play(game1, 3);
+
+        int expected = 3;
+        int actual = player.sumGenre(game1.getGenre());
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSumGenreInSeveralGames() {
+
+        Player player = new Player("Petya");
+        player.installGame(game1);
+        player.installGame(game2);
+        player.installGame(game3);
+        player.play(game1, 3);
+        player.play(game2, 1);
+        player.play(game3, 4);
+
+        int expected = 8;
+        int actual = player.sumGenre(game1.getGenre());
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSumGenreInSeveralGamesWithRepeat() {
+
+        Player player = new Player("Petya");
+        player.installGame(game1);
+        player.installGame(game2);
+        player.installGame(game3);
+        player.play(game1, 3);
+        player.play(game2, 1);
+        player.play(game3, 4);
+        player.play(game1, 2);
+
+        int expected = 10;
+        int actual = player.sumGenre(game1.getGenre());
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldNotSumDifferentGenre() {
+
+        Player player = new Player("Petya");
+        player.installGame(game1);
+        player.installGame(game2);
+        player.installGame(game4);
+        player.play(game1, 3);
+        player.play(game2, 1);
+        player.play(game4, 4);
+
+        int expected = 4;
+        int actual = player.sumGenre(game1.getGenre());
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldNotSumGenreOfDifferentPlayer() {
+
+        Player player = new Player("Petya");
+        player.installGame(game1);
+        player.installGame(game2);
+        player.play(game1, 3);
+        player.play(game2, 1);
+
+        Player player1 = new Player("Ann");
+        player1.installGame(game2);
+        player1.play(game2, 4);
+
+        int expected = 4;
+        int actual = player.sumGenre(game2.getGenre());
+
+        assertEquals(expected, actual);
+    }
 
     @Test
     public void shouldShowMostPlayedGame() {
